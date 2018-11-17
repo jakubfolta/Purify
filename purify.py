cb7a2a8a588cdd5e33e7e02f37118670ded86070
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-11-17 14:26:59 +0100

    other solutions - finished

--- purify.py
+++ purify.py
@@ -6,3 +6,18 @@ def purify(group):
   return package
 
 print(purify([1, 2, 4, 4, 5, 7, 8, 9, 9, 6]))
+
+
+def purifying(pack):
+  for number in pack[:]:
+    if number % 2 != 0:
+      pack.remove(number)
+  return pack
+
+print(purifying([1, 1, 2, 3, 3, 4, 5, 6, 6, 7, 7]))
+
+
+def purify(horde):
+  return [number for number in horde if number % 2 == 0] 
+
+print(purify([1, 2, 2, 3, 3, 6, 6]))
