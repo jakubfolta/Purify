c0ab648e9704b656b5658122db4a8c44ee707808
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-11-17 13:29:41 +0100

    work in progress

--- purify.py
+++ purify.py
@@ -1,1 +1,8 @@
 def purify(group):
+  package = group
+  for number in group:
+    if number % 2 != 0:
+      package.remove(number)
+  return package
+
+print(purify([1, 2, 4, 4, 5, 7, 8, 9, 9, 6]))
